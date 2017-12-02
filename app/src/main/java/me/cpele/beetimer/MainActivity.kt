b1d194895e333cbc7465618fcbea435e88cdb6ea
cpcp <geekarist@users.noreply.github.com>
2017-12-02 20:09:11 +0100

    Start oauth process

--- app/src/main/java/me/cpele/beetimer/MainActivity.kt
+++ app/src/main/java/me/cpele/beetimer/MainActivity.kt
@@ -1,12 +1,27 @@
 package me.cpele.beetimer
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    val BMNDR_CLIENT_ID = "30zxgk213ellu3dj730wto3qj"
+    val BMNDR_REDIRECT_URI = "beetimer://auth_callback"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
+
+        val signInButton = main_bt_sign_in
+        signInButton.setOnClickListener {
+            val uri = Uri.parse("https://www.beeminder.com/apps/authorize?" +
+                    "client_id=$BMNDR_CLIENT_ID&redirect_uri=$BMNDR_REDIRECT_URI&response_type=token")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
+
 }
