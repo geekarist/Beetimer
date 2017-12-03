50295191e2052a1b1d5669fc3cc8319650688097
cpcp <geekarist@users.noreply.github.com>
2017-12-03 18:13:38 +0100

    Fetch goal slugs

--- app/src/main/java/me/cpele/beetimer/MainActivity.kt
+++ app/src/main/java/me/cpele/beetimer/MainActivity.kt
@@ -6,6 +6,13 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 private const val BMNDR_CLIENT_ID = "30zxgk213ellu3dj730wto3qj"
 private const val BMNDR_REDIRECT_URI = "beetimer://auth_callback"
@@ -37,7 +44,34 @@ class MainActivity : AppCompatActivity() {
         val host = data?.host
         if (action == Intent.ACTION_VIEW && scheme == "beetimer" && host == "auth_callback") {
             val accessToken = data.getQueryParameter("access_token")
-            Toast.makeText(this@MainActivity, "Token: $accessToken", Toast.LENGTH_LONG).show()
+            fetchGoals(accessToken)
         }
     }
+
+    private fun fetchGoals(accessToken: String) {
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://www.beeminder.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        val api = retrofit.create(BeeminderApi::class.java)
+        api.getUser(accessToken).enqueue(object: Callback<User> {
+            override fun onFailure(call: Call<User>?, t: Throwable?) {
+                TODO("not implemented")
+            }
+
+            override fun onResponse(call: Call<User>?, response: Response<User>?) {
+                response?.body()?.goals?.apply {
+                    Toast.makeText(this@MainActivity, toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+    }
 }
+
+interface BeeminderApi {
+    @GET("/api/v1/users/me.json")
+    fun getUser(@Query("access_token") accessToken: String): Call<User>
+}
+
+data class User(val username: String, val goals: List<String>)
+
