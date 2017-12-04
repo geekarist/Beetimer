46b8a1be989e67ee666dd8a54407fd9d2d5c6bce
cpcp <geekarist@users.noreply.github.com>
2017-12-04 13:46:32 +0100

    Display days before derailment

--- app/src/main/java/me/cpele/beetimer/Goal.kt
+++ app/src/main/java/me/cpele/beetimer/Goal.kt
@@ -1,3 +1,26 @@
 package me.cpele.beetimer
 
-data class Goal(val slug: String, val title: String)
+import android.support.annotation.ColorRes
+
+data class Goal(
+        val slug: String?,
+        val title: String?,
+        val rate: String?,
+        val delta_text: String?,
+        val losedate: Long?,
+        private val lane: Int?,
+        private val yaw: Int?) {
+
+    val color: Int
+        @ColorRes
+        get() {
+            if (lane == null || yaw == null) return android.R.color.white
+
+            val laneTimesYaw = lane * yaw
+            if (laneTimesYaw > 1) return R.color.green
+            else if (laneTimesYaw == 1) return R.color.blue
+            else if (laneTimesYaw == -1) return R.color.orange
+            else if (laneTimesYaw <= -2) return R.color.red
+            return android.R.color.white
+        }
+}
--- app/src/main/java/me/cpele/beetimer/GoalViewHolder.kt
+++ app/src/main/java/me/cpele/beetimer/GoalViewHolder.kt
@@ -3,13 +3,44 @@ package me.cpele.beetimer
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
+import java.util.concurrent.TimeUnit
 
 class GoalViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
     fun bind(goal: Goal) {
         val idView: TextView = itemView.findViewById(R.id.item_id)
         idView.text = goal.slug
 
-        val titleview: TextView = itemView.findViewById(R.id.item_title)
-        titleview.text = goal.title
+        val titleView: TextView = itemView.findViewById(R.id.item_title)
+        titleView.text = goal.title
+
+        val rateView: TextView = itemView.findViewById(R.id.item_rate)
+        rateView.text = goal.rate
+
+        val bareMinView: TextView = itemView.findViewById(R.id.item_bare_min)
+        bareMinView.text = goal.delta_text
+
+        @Suppress("DEPRECATION")
+        goal.losedate?.let {
+
+            val color = goal.color
+
+            val numDerailDays =
+                    TimeUnit.SECONDS.toDays(goal.losedate) -
+                            TimeUnit.MILLISECONDS.toDays(System.currentTimeMillis())
+            val derailDaysView: TextView = itemView.findViewById(R.id.item_derail_days)
+            derailDaysView.text = numDerailDays.toString()
+            derailDaysView.setBackgroundColor(itemView.context.resources.getColor(color))
+
+            val numDerailHours =
+                    TimeUnit.SECONDS.toHours(goal.losedate) -
+                            TimeUnit.MILLISECONDS.toHours(System.currentTimeMillis()) -
+                            TimeUnit.DAYS.toHours(numDerailDays)
+            val derailHoursView: TextView = itemView.findViewById(R.id.item_derail_time)
+            val numDerailMin = 51
+            val numDerailSec = 16
+            derailHoursView.text = itemView.context.getString(
+                    R.string.item_derail_time, numDerailHours, numDerailMin, numDerailSec)
+            derailHoursView.setBackgroundColor(itemView.context.resources.getColor(color))
+        }
     }
 }
