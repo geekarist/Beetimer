eb3868ef2956acaf1a18a44354e8351cbfd858ab
cpcp <geekarist@users.noreply.github.com>
2017-12-22 18:25:37 +0100

    Remove unnecessary safe calls

--- app/src/main/java/me/cpele/beetimer/GoalViewHolder.kt
+++ app/src/main/java/me/cpele/beetimer/GoalViewHolder.kt
@@ -9,66 +9,64 @@ import java.util.concurrent.TimeUnit
 
 class GoalViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
 
-    private val mContext: Context
+    private val context: Context
         get() = itemView.context
 
     private lateinit var stopWatch: StopWatch
 
-    private var handler: Handler? = null
-    private var runnable: Runnable? = null
+    private lateinit var handler: Handler
+    private lateinit var runnable: Runnable
 
     fun bind(goal: Goal) {
         itemView.item_id.text = goal.slug
         itemView.item_title.text = goal.title
-        itemView.item_rate.text = mContext.getString(R.string.item_rate, goal.rate, goal.runits)
+        itemView.item_rate.text = context.getString(R.string.item_rate, goal.rate, goal.runits)
         itemView.item_bare_min.text = goal.limsum
 
         stopWatch = StopWatch()
         itemView.item_timer.setOnClickListener { stopWatch.toggle() }
         handler = Handler()
-        runnable = object: Runnable {
+        runnable = object : Runnable {
             override fun run() {
                 itemView.item_timer.text = stopWatch.format()
-                handler?.postDelayed(this, 1000)
+                handler.postDelayed(this, 1000)
             }
         }
-        handler?.post(runnable)
+        handler.post(runnable)
+
+        val color = goal.color
+
+        val numDerailDays =
+                TimeUnit.SECONDS.toDays(goal.losedate) -
+                        TimeUnit.MILLISECONDS.toDays(System.currentTimeMillis())
+        itemView.item_derail_days.text = context.getString(R.string.item_derail_date, numDerailDays)
+        @Suppress("DEPRECATION")
+        itemView.item_derail_days.setBackgroundColor(context.resources.getColor(color))
+
+        val numDerailHours =
+                TimeUnit.SECONDS.toHours(goal.losedate) -
+                        TimeUnit.MILLISECONDS.toHours(System.currentTimeMillis()) -
+                        TimeUnit.DAYS.toHours(numDerailDays)
+
+        val numDerailMin = TimeUnit.SECONDS.toMinutes(goal.losedate) -
+                TimeUnit.MILLISECONDS.toMinutes(System.currentTimeMillis()) -
+                TimeUnit.DAYS.toMinutes(numDerailDays) -
+                TimeUnit.HOURS.toMinutes(numDerailHours)
+
+        val numDerailSec = TimeUnit.SECONDS.toSeconds(goal.losedate) -
+                TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()) -
+                TimeUnit.DAYS.toSeconds(numDerailDays) -
+                TimeUnit.HOURS.toSeconds(numDerailHours) -
+                TimeUnit.MINUTES.toSeconds(numDerailMin)
+
+        itemView.item_derail_time.text = context.getString(
+                R.string.item_derail_time, numDerailHours, numDerailMin, numDerailSec)
 
         @Suppress("DEPRECATION")
-        goal.losedate?.let {
-
-            val color = goal.color
-
-            val numDerailDays =
-                    TimeUnit.SECONDS.toDays(goal.losedate) -
-                            TimeUnit.MILLISECONDS.toDays(System.currentTimeMillis())
-            itemView.item_derail_days.text = mContext.getString(R.string.item_derail_date, numDerailDays)
-            itemView.item_derail_days.setBackgroundColor(mContext.resources.getColor(color))
-
-            val numDerailHours =
-                    TimeUnit.SECONDS.toHours(goal.losedate) -
-                            TimeUnit.MILLISECONDS.toHours(System.currentTimeMillis()) -
-                            TimeUnit.DAYS.toHours(numDerailDays)
-
-            val numDerailMin = TimeUnit.SECONDS.toMinutes(goal.losedate) -
-                    TimeUnit.MILLISECONDS.toMinutes(System.currentTimeMillis()) -
-                    TimeUnit.DAYS.toMinutes(numDerailDays) -
-                    TimeUnit.HOURS.toMinutes(numDerailHours)
-
-            val numDerailSec = TimeUnit.SECONDS.toSeconds(goal.losedate) -
-                    TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()) -
-                    TimeUnit.DAYS.toSeconds(numDerailDays) -
-                    TimeUnit.HOURS.toSeconds(numDerailHours) -
-                    TimeUnit.MINUTES.toSeconds(numDerailMin)
-
-            itemView.item_derail_time.text = mContext.getString(
-                    R.string.item_derail_time, numDerailHours, numDerailMin, numDerailSec)
-
-            itemView.item_derail_time.setBackgroundColor(mContext.resources.getColor(color))
-        }
+        itemView.item_derail_time.setBackgroundColor(context.resources.getColor(color))
     }
 
     fun release() {
-        handler?.removeCallbacks(runnable)
+        handler.removeCallbacks(runnable)
     }
 }
