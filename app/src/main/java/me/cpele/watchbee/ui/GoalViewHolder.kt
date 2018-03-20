9e37bab3a522bceddf6f93e3350067ec2c04fbc1
cpcp <geekarist@users.noreply.github.com>
2018-03-20 13:43:26 +0100

    GH-15: Add reference to listener in layout

--- app/src/main/java/me/cpele/watchbee/ui/GoalViewHolder.kt
+++ app/src/main/java/me/cpele/watchbee/ui/GoalViewHolder.kt
@@ -28,53 +28,12 @@ class GoalViewHolder(
 
     fun bind(goalTiming: GoalTiming) {
 
-        itemBinding.model = goalTiming.goal
+        itemBinding.model = goalTiming
+        itemBinding.listener = this
 
         stopwatch = goalTiming.stopwatch
-        itemView.item_timer.setOnClickListener {
-            stopwatch.toggle()
-            listener.onPersist(goalTiming)
-        }
-        itemView.item_timer.setOnLongClickListener {
-            val wasRunning = stopwatch.running
-            stopwatch.stop()
-            val timePickerDialog = TimePickerDialog(
-                    context,
-                    { _: TimePicker, hours: Int, minutes: Int ->
-                        stopwatch.set(hours, minutes)
-                        if (wasRunning) stopwatch.start()
-                        listener.onPersist(goalTiming)
-                    },
-                    stopwatch.hours,
-                    stopwatch.minutes,
-                    true
-            )
-            timePickerDialog.setOnCancelListener {
-                if (wasRunning) stopwatch.start()
-                listener.onPersist(goalTiming)
-            }
-            timePickerDialog.show()
-            true
-        }
 
         attach()
-
-        itemView.item_reset.setOnClickListener {
-            AlertDialog.Builder(context)
-                    .setMessage("Do you really want to reset this stopwatch?")
-                    .setPositiveButton(android.R.string.ok, { _, _ ->
-                        stopwatch.clear()
-                        listener.onPersist(goalTiming)
-                    })
-                    .setNeutralButton(android.R.string.cancel, { dialog, _ ->
-                        dialog.cancel()
-                    })
-                    .show()
-        }
-
-        itemView.item_submit.setOnClickListener {
-            listener.onSubmit(goalTiming)
-        }
     }
 
     fun attach() {
@@ -86,6 +45,50 @@ class GoalViewHolder(
             handler.postDelayed(runnable, 1000)
         }
         handler.post(runnable)
+    }
+
+    fun onClickTimer(goalTiming: GoalTiming) {
+        stopwatch.toggle()
+        listener.onPersist(goalTiming)
+    }
+
+    fun onLongClickTimer(goalTiming: GoalTiming): Boolean {
+        val wasRunning = stopwatch.running
+        stopwatch.stop()
+        val timePickerDialog = TimePickerDialog(
+                context,
+                { _: TimePicker, hours: Int, minutes: Int ->
+                    stopwatch.set(hours, minutes)
+                    if (wasRunning) stopwatch.start()
+                    listener.onPersist(goalTiming)
+                },
+                stopwatch.hours,
+                stopwatch.minutes,
+                true
+        )
+        timePickerDialog.setOnCancelListener {
+            if (wasRunning) stopwatch.start()
+            listener.onPersist(goalTiming)
+        }
+        timePickerDialog.show()
+        return true
+    }
+
+    fun onClickReset(goalTiming: GoalTiming) {
+        AlertDialog.Builder(context)
+                .setMessage("Do you really want to reset this stopwatch?")
+                .setPositiveButton(android.R.string.ok, { _, _ ->
+                    stopwatch.clear()
+                    listener.onPersist(goalTiming)
+                })
+                .setNeutralButton(android.R.string.cancel, { dialog, _ ->
+                    dialog.cancel()
+                })
+                .show()
+    }
+
+    fun onClickSubmit(goalTiming: GoalTiming) {
+        listener.onSubmit(goalTiming)
     }
 
     fun detach() {
