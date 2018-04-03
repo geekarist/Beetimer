beae523dab3300c03da919436ab72dfc11372db7
cpcp <geekarist@users.noreply.github.com>
2018-04-03 13:33:57 +0200

    GH-4: Dynamic start/stop icon

--- app/src/main/java/me/cpele/watchbee/ui/DetailViewModel.kt
+++ app/src/main/java/me/cpele/watchbee/ui/DetailViewModel.kt
@@ -1,8 +1,12 @@
 package me.cpele.watchbee.ui
 
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MediatorLiveData
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
+import android.content.Context
+import android.graphics.drawable.Drawable
+import me.cpele.watchbee.R
 import me.cpele.watchbee.domain.GoalTiming
 import me.cpele.watchbee.repository.BeeRepository
 
@@ -12,6 +16,18 @@ class DetailViewModel(
 ) : ViewModel() {
 
     val goalTiming: LiveData<GoalTiming> = beeRepository.asyncFindGoalTimingBySlug(slug)
+
+    fun startStopDrawable(context: Context): LiveData<Drawable> {
+        val drawableData = MediatorLiveData<Drawable>()
+        drawableData.addSource(goalTiming, {
+            drawableData.value =
+                    when (it?.stopwatch?.running) {
+                        false -> context.getDrawable(R.drawable.ic_play_arrow_black_24dp)
+                        else -> context.getDrawable(R.drawable.ic_pause_black_24dp)
+                    }
+        })
+        return drawableData
+    }
 
     fun forceRefresh() {
         beeRepository.forceRefreshGoalTimingBySlug(slug)
