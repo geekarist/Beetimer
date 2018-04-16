2718831edbc922a183fb38ac3ddd5196a20a9d11
cpele <christophe.pele@gmail.com>
2018-04-16 07:10:01 +0200

    GH-4: Interpret timestamp as seconds, not milliseconds

--- app/src/main/java/me/cpele/watchbee/api/EpochTypeAdapter.kt
+++ app/src/main/java/me/cpele/watchbee/api/EpochTypeAdapter.kt
@@ -4,14 +4,19 @@ import com.google.gson.TypeAdapter
 import com.google.gson.stream.JsonReader
 import com.google.gson.stream.JsonWriter
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 class EpochTypeAdapter : TypeAdapter<Date>() {
 
     override fun write(out: JsonWriter?, value: Date?) {
-        out?.value(value?.time)
+        val milTime = value?.time ?: 0
+        val secTime = TimeUnit.MILLISECONDS.toSeconds(milTime)
+        out?.value(secTime)
     }
 
     override fun read(input: JsonReader?): Date {
-        return input?.nextLong()?.let { Date(it) } ?: Date()
+        return input?.nextLong()?.let {
+            Date(TimeUnit.SECONDS.toMillis(it))
+        } ?: Date(0)
     }
 }
